0a8758070ea59b5816e5045d3919ce379cc784e1
Josh Adams <josh@waxcorp.com>
2011-04-17 15:47:53 -0700

    provide test code in test run output

--- test/run.sh
+++ test/run.sh
@@ -8,6 +8,8 @@ for test in $tests; do
   cat $test >> $tmp
 
   echo INFO: running test $test from $tmp
+  cat -n $tmp | sed -e 's/^/INFO: /'
+  echo
   chuck src/mondrum.ck test/lib/test-setup.ck $tmp
   echo -e "INFO: test $test complete\n"
 
